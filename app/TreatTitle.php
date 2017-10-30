8fee83790d978a5cf0d9ce793c5cae561bb50f83
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-10-30 17:17:11 +0300

    Made age group addable

--- app/TreatTitle.php
+++ app/TreatTitle.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TreatTitle extends Model
 {
     //
-    protected $fillable = ['title', 'guide'];
+    protected $fillable = ['title', 'guide', 'age_group_id'];
 
     public function age_group(){
     	return $this->belongsTo('App\AgeGroup');
