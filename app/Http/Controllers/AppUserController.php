35bcb659de6c5e131c30cb94e412478cabaf0ce7
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-12-10 19:25:04 +0300

    Added updates to the appuser

--- app/Http/Controllers/AppUserController.php
+++ app/Http/Controllers/AppUserController.php
@@ -17,6 +17,15 @@ class AppUserController extends Controller
     	$appuser = AppUser::where('display_no', $display_no)->first();
     	if ($appuser) {
     		$appuser->phone_id = $request->input('phone_id');
+            $appuser->opened_at = $request->input('opened_at');
+            $appuser->brand = $request->input('brand');
+            $appuser->model = $request->input('model');
+            $appuser->device = $request->input('device');
+            $appuser->android_version = $request->input('android_version');
+            $appuser->display_no = $request->input('display_no');
+            $appuser->android_sdk = $request->input('android_sdk');
+            $appuser->android_release = $request->input('android_release');
+            $appuser->device_model = $request->input('device_model');
             $appuser->save();
     	}else{
     		$appuser = AppUser::create($request->all());
