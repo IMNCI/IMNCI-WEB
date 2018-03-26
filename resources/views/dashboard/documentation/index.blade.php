e7d0d504cfd06ee16bb883a14411394cd57a17f0
Chrispine Otaalo <john.otaalo@strathmore.edu>
2018-03-26 13:52:00 +0300

    Added last modified statement

--- resources/views/dashboard/documentation/index.blade.php
+++ resources/views/dashboard/documentation/index.blade.php
@@ -32,7 +32,7 @@
                     </div>
                     <div class="file-name">
                         <p>{{ substr($file, strpos($file, "/") + 1) }}</p>
-                        <small>{{ date('M d, Y', Storage::lastModified($file)) }}</small>
+                        Last Modified: <small>{{ date('M d, Y', Storage::lastModified($file)) }}</small>
                     </div>
                 </a>
             </div>
