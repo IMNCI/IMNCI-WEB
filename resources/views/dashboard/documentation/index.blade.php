82d9498b16b0a90f7358a03fa8c27010a1eb2526
Chrispine Otaalo <john.otaalo@strathmore.edu>
2018-03-26 12:50:14 +0300

    Added the documentation page

--- resources/views/dashboard/documentation/index.blade.php
+++ resources/views/dashboard/documentation/index.blade.php
@@ -1,9 +1,43 @@
 @extends('layouts.dashboard')
 
+@section('page_css')
+	@parent
+    <style>
+        .file-name p{
+            overflow: hidden;
+        }
+
+        .file-box{
+            width: 0;
+        }
+    </style>
+@stop
+
 @section('title')
 {{ __('dashboard.documentation') }}
 @stop
+@section('content')
+<div class = 'row'>
+    <?php
+        $files = Storage::files('documents');
+    ?>
 
-<?php
-phpinfo();
-?>
+    @forelse($files as $file)
+        <div class="col-md-3">
+            <div class="file">
+                <a target = '_blank' href="{{ Storage::url($file) }}">
+                    <span class="corner"></span>
+                    <div class="icon">
+                        <i class="fa fa-file-pdf-o"></i>
+                    </div>
+                    <div class="file-name">
+                        <p>{{ substr($file, strpos($file, "/") + 1) }}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    @empty
+
+    @endforelse
+</div>
+@stop
